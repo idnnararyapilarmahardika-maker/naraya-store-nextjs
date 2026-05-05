4b5e4aff5ef36b7ad1659ebcc7c3a5575d67b636
Nara <idnnararyapilarmahardika@gmail.com>
2026-05-05 12:25:16 +0700

    Delete nararya-store directory

--- nararya-store/components/SkeletonCard.js
+++
@@ -1,9 +0,0 @@
-export default function SkeletonCard() {
-    return (
-        <div className="border p-4 rounded-lg shadow-sm space-y-3 bg-white">
-            <div className="h-6 w-3/4 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-4 w-full bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-4 w-5/6 bg-gray-200 rounded animate-pulse"></div>
-        </div>
-    );
-}
